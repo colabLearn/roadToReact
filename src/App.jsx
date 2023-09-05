dd24e070d99d6b99be11c749025521bc341c2b3f
User <tunji4physics@gmail.com>
2023-09-05 15:29:52 +0100

    using an array's  map() method and assigning HTML attributes to render element of a list dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import * as React from 'react'
+//import * as React from 'react'
 
+const list = [
+  {
+    title: 'Road To React',
+  url: 'https://reactjs.org',
+  author: 'Adetunji Adeniran',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
 
-  //Variable definition
-  //const title = 'React'
-  // const welcome = {
-  //   greeting: 'Hey',
-  //   title: 'React',
-  // }
-
-  function getTitle(title){
-    return title
+  {
+    title: 'Redux',
+  url: 'https://react.js.org',
+  author: 'Babatunde Adeniran',
+  num_comments: 2,
+  points: 4,
+  objectID: 1,
   }
+
+]
 
 function App() {
 
@@ -18,12 +27,34 @@ function App() {
   return (
     
     <div>
-      <h1>
-        {getTitle('React')}
-      </h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor='search'> Search</label>
       <input id="search" type="text"></input>
+
+      <hr />
+      <table>
+      <tr>
+          <th><strong>Title</strong></th>
+          <th><strong>Author</strong></th>
+          <th><strong>Num of Comments</strong></th>
+          <th><strong>Points</strong></th>
+        </tr>
+        {list.map(function(item){
+          return          (
+              <tr key={item.objectID}>
+              <td><a href={item.url}>{item.title}</a></td>
+              <td>{item.author}</td>
+              <td>{item.num_comments}</td>
+              <td>{item.points}</td>
+            </tr>
+           
+            /*</div><li key={item.objectID}>{item.title}</li>*/
+
+          )  
+
+        })}
+      </table>
     </div>
   );
   
