3e4705e2c7300b245c6fda20171648629465934e
User <tunji4physics@gmail.com>
2023-09-07 19:02:36 +0100

    Extract some more fucntionalities from App component and create Search component as a child of App component -- Also add JPEG file illustrating current design of our react app

--- src/App.jsx
+++ src/App.jsx
@@ -29,14 +29,24 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor='search'> Search</label>
-      <input id="search" type="text"></input>
+     <Search />
 
       <hr />
       <List/>
     </div>
   );
   
+}
+
+function Search(){
+  return(
+     <div>
+       <label htmlFor='search'> Search</label>
+     <input id="search" type="text"></input>
+
+     </div>
+
+  )
 }
 function List (){
   return (
